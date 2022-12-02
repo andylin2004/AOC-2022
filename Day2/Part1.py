f788b70d2102762f9647de43c9c8ed087d2c314f
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-12-02 00:15:14 -0500

    disgusting to read that problem through and think abt it

--- Day2/Part1.py
+++ Day2/Part1.py
@@ -1,6 +1,27 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
 
+points = 0
 for line in input:
-    
+    parts = line.split()
+    if parts[1] == 'X': # rock
+        points += 1
+        if parts[0] == 'A': # rock
+            points += 3
+        elif parts[0] == 'C': #scissor
+            points += 6
+    elif parts[1] == 'Y': # paper
+        points += 2
+        if parts[0] == 'A': # rock
+            points += 6
+        elif parts[0] == 'B': #paper
+            points += 3
+    elif parts[1] == 'Z': #scissors
+        points += 3
+        if parts[0] == 'B': # paper
+            points += 6
+        elif parts[0] == 'C': #scissor
+            points += 3
+
+print(points)
--- Day2/Part2.py
+++ Day2/Part2.py
@@ -1,6 +1,32 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
 
+points = 0
 for line in input:
-    
+    parts = line.split()
+    if parts[1] == 'X': # lose
+        if parts[0] == 'A': # rock
+            points += 3
+        elif parts[0] == 'B': # paper
+            points += 1
+        elif parts[0] == 'C': # scissor
+            points += 2
+    elif parts[1] == 'Y': # draw
+        points += 3
+        if parts[0] == 'A': # rock
+            points += 1
+        elif parts[0] == 'B': # paper
+            points += 2
+        elif parts[0] == 'C': # scissor
+            points += 3
+    elif parts[1] == 'Z': #win
+        points += 6
+        if parts[0] == 'A': # rock
+            points += 2
+        elif parts[0] == 'B': # paper
+            points += 3
+        elif parts[0] == 'C': # scissor
+            points += 1
+
+print(points)
