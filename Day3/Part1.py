1a86d9141010f0430ac5a58d02f08026b5357911
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-12-03 00:20:15 -0500

    day 3 done but reading comprehension

--- Day3/Part1.py
+++ Day3/Part1.py
@@ -1,6 +1,26 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
 
+rucksack_a = []
+common = None
+total = 0
 for line in input:
-    
+    rucksack_a = []
+    common = None
+    add_chars = len(line) // 2
+    i = 0
+    for char in line:
+        if i < add_chars:
+            rucksack_a.append(char)
+            i += 1
+        else:
+            if char in rucksack_a:
+                common = char
+    # print(common)
+    if ord(common) >= 97:
+        total += ord(common) - 96
+    else:
+        total += ord(common) - 38
+
+print(total)
--- Day3/Part2.py
+++ Day3/Part2.py
@@ -1,6 +1,31 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
 
+rucksack_a = []
+common_ab = []
+common_all = None
+total = 0
+line_count = 0
 for line in input:
-    
+    if line_count % 3 == 0:
+        rucksack_a = [x for x in line]
+        print(rucksack_a)
+    elif line_count % 3 == 1:
+        for char in line:
+            if char in rucksack_a:
+                common_ab.append(char)
+    else:
+        for char in line:
+            if char in common_ab:
+                common_all = char
+        if ord(common_all) >= 97:
+            total += ord(common_all) - 96
+        else:
+            total += ord(common_all) - 38
+        rucksack_a = []
+        common_ab = []
+        common_all = None
+    line_count += 1
+
+print(total)
