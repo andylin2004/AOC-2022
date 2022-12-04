58c0d3a3d0dbc090fc2ea20e95df4a144fab794e
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-12-04 00:29:57 -0500

    how did i manage to do so badly today

--- Day4/Part1.py
+++ Day4/Part1.py
@@ -1,6 +1,14 @@
 f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
 
+pairs = 0
 for line in input:
-    
+    print(line)
+    line_parts = line.split(",")
+    left_range = line_parts[0].split("-")
+    right_range = line_parts[1].split("-")
+    if (int(left_range[0]) <= int(right_range[0]) and int(right_range[1]) <= int(left_range[1])) or (int(right_range[0]) <= int(left_range[0]) and int(left_range[1]) <= int(right_range[1])):
+        pairs += 1
+
+print(pairs)
