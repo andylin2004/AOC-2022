1632d8566ad5b6aff6de7aded6950d6f51f2ffed
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-12-05 00:43:24 -0500

    i always tell myself id do better on the next day...i guess not (day 5 done)

--- Day5/Part1.py
+++ Day5/Part1.py
@@ -1,6 +1,30 @@
+import re
+from collections import deque
+
 f = open("input.txt", 'r')
 f = open("test_input.txt", 'r')
-input = f.read()
+input = f.read().splitlines()
+
+stacks = [deque() for _ in input[input.index("") - 1].split()]
+move = False
+pattern = re.compile("move ([0-9]+) from ([0-9]) to ([0-9])")
 
 for line in input:
-    
+    if line == "":
+        move = True
+        print(stacks)
+    elif move:
+        m = pattern.match(line)
+        qty = int(m.group(1))
+        origin = int(m.group(2)) - 1
+        end = int(m.group(3)) - 1
+        for _ in range(qty):
+            stacks[end].append(stacks[origin].pop())
+    elif not move and not line.startswith("move"):
+        split = []
+        for i in range(0, len(line), 4):
+            print(line[i+1])
+            if line[i+1] != " " and line[i+1] != "" and not line[i+1].isdigit():
+                stacks[i // 4].appendleft(line[i+1])
+        
+print("".join([x[-1] for x in stacks]))
--- Day5/Part2.py
+++ Day5/Part2.py
@@ -1,6 +1,35 @@
+import re
+from collections import deque
+
 f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
+
+stacks = [deque() for _ in input[input.index("") - 1].split()]
+move = False
+pattern = re.compile("move ([0-9]+) from ([0-9]) to ([0-9])")
 
 for line in input:
-    
+    if line == "":
+        move = True
+        print(stacks)
+    elif move:
+        m = pattern.match(line)
+        qty = int(m.group(1))
+        origin = int(m.group(2)) - 1
+        end = int(m.group(3)) - 1
+        deq = deque()
+        for _ in range(qty):
+            deq.append(stacks[origin].pop())
+        print(deq)
+        for _ in range(qty):
+            stacks[end].append(deq.pop())
+        print(stacks, "e")
+    elif not move and not line.startswith("move"):
+        split = []
+        for i in range(0, len(line), 4):
+            print(line[i+1])
+            if line[i+1] != " " and line[i+1] != "" and not line[i+1].isdigit():
+                stacks[i // 4].appendleft(line[i+1])
+        
+print("".join([x[-1] for x in stacks]))
