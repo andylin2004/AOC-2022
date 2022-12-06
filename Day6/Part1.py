12c91cc738442cbf4ba8b04463f4c9ded8aba526
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-12-06 00:10:23 -0500

    bruh why am i so slow

--- Day6/Part1.py
+++ Day6/Part1.py
@@ -2,5 +2,13 @@ f = open("input.txt", 'r')
 f = open("test_input.txt", 'r')
 input = f.read()
 
-for line in input:
-    
+run = True
+for i in range(len(input)):
+    for v in range(14):
+        if input[i:i+14].count(input[i+v]) > 1:
+            break
+        if v == 13:
+            print(i+14)
+            run = False
+    if not run:
+        break
--- Day6/Part2.py
+++ Day6/Part2.py
@@ -2,5 +2,13 @@ f = open("input.txt", 'r')
 f = open("test_input.txt", 'r')
 input = f.read()
 
-for line in input:
-    
+run = True
+for i in range(len(input)):
+    for v in range(4):
+        if input[i:i+4].count(input[i+v]) > 1:
+            break
+        if v == 3:
+            print(i+4)
+            run = False
+    if not run:
+        break
