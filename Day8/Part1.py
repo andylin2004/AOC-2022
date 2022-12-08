a00e7e5e07a6c43b9e6ce4003603e51597eac389
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-12-08 00:23:22 -0500

    today was a good day day 8

--- Day8/Part1.py
+++ Day8/Part1.py
@@ -1,6 +1,42 @@
 f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
 
-for line in input:
-    
+total_visible = 0
+for line in range(len(input)):
+    for char in range(len(input[line])):
+        if line == 0 or line == len(input) - 1:
+            total_visible += 1
+        elif char == 0 or char == len(input[line]) - 1:
+            total_visible += 1
+        else:
+            searching = True
+            for eval_line in range(line - 1, -1, -1):
+                if int(input[eval_line][char]) >= int(input[line][char]):
+                    break
+                elif eval_line == 0:
+                    total_visible += 1
+                    searching = False
+            if searching:
+                for eval_line in range(line + 1, len(input)):
+                    if int(input[eval_line][char]) >= int(input[line][char]):
+                        break
+                    elif eval_line == len(input) - 1:
+                        total_visible += 1
+                        searching = False
+            if searching:
+                for eval_char in range(char - 1, -1, -1):
+                    if int(input[line][eval_char]) >= int(input[line][char]):
+                        break
+                    elif eval_char == 0:
+                        total_visible += 1
+                        searching = False
+            if searching:
+                for eval_char in range(char + 1, len(input[line])):
+                    if int(input[line][eval_char]) >= int(input[line][char]):
+                        break
+                    elif eval_char == len(input[line]) - 1:
+                        total_visible += 1
+                        searching = False
+                
+print(total_visible)
--- Day8/Part2.py
+++ Day8/Part2.py
@@ -1,6 +1,43 @@
 f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
 
-for line in input:
-    
+max_sceneic = 0
+for line in range(len(input)):
+    for char in range(len(input[line])):
+        scene_score = 1
+        counter = 0
+        for eval_line in range(line - 1, -1, -1):
+            if int(input[eval_line][char]) >= int(input[line][char]):
+                counter += 1
+                break
+            else:
+                counter += 1
+        scene_score *= counter
+        counter = 0
+        for eval_line in range(line + 1, len(input)):
+            if int(input[eval_line][char]) >= int(input[line][char]):
+                counter += 1
+                break
+            else:
+                counter += 1
+        scene_score *= counter
+        counter = 0
+        for eval_char in range(char - 1, -1, -1):
+            if int(input[line][eval_char]) >= int(input[line][char]):
+                counter += 1
+                break
+            else:
+                counter += 1
+        scene_score *= counter
+        counter = 0
+        for eval_char in range(char + 1, len(input[line])):
+            if int(input[line][eval_char]) >= int(input[line][char]):
+                counter += 1
+                break
+            else:
+                counter += 1
+        scene_score *= counter
+        max_sceneic = max(max_sceneic, scene_score)
+                
+print(max_sceneic)
