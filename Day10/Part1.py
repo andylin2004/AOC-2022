b5f30df9027833c23756dca7f816ee60d8a6db84
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-12-10 01:00:23 -0500

    day 10 done kinda slow but at least no burnout

--- Day10/Part1.py
+++ Day10/Part1.py
@@ -1,6 +1,30 @@
 f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
 
-for line in input:
+val = 1
+strength = 0
+line = 0
+cycle = 1
+start_exec_cycle = None
+strength_count = 0
+
+while line < len(input):
+    if cycle % 40 == 20:
+        strength += cycle * val
+        strength_count += 1
+        if strength_count == 6:
+            break
+    if start_exec_cycle is not None and cycle - start_exec_cycle  == 1:
+        val += int(input[line][5:])
+        start_exec_cycle = None
+        line += 1
+    elif start_exec_cycle is None:
+        if input[line][:4] == "addx":
+            start_exec_cycle = cycle
+        elif input[line][:4] == "noop":
+            line += 1
+
+    cycle += 1
     
+print(strength)
--- Day10/Part2.py
+++ Day10/Part2.py
@@ -1,6 +1,31 @@
 f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
 
-for line in input:
-    
+position = 1
+line = 0
+cycle = 1
+start_exec_cycle = None
+
+screen = [[" " for h in range(40)] for x in range(6)]
+
+while line < len(input):
+    if position % 40 == cycle % 40 or position % 40 + 1 == cycle % 40 or position % 40 + 2 == cycle % 40:
+        print(cycle - 1)
+        screen[(cycle - 1) // 40][(cycle - 1) % 40] = "#"
+    if start_exec_cycle is not None and cycle - start_exec_cycle  == 1:
+        position += int(input[line][5:])
+        start_exec_cycle = None
+        line += 1
+    elif start_exec_cycle is None:
+        if input[line][:4] == "addx":
+            start_exec_cycle = cycle
+        elif input[line][:4] == "noop":
+            line += 1
+
+    cycle += 1
+
+for line in screen:
+    for char in line:
+        print(char, end="")
+    print()
